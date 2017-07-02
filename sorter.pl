60646849217db49c58d2c465daad42bcee425e31
dkurchigin <kurchigin.dmitry@yandex.ru>
2017-07-02 12:41:42 +0300

    add check OS-type win&linux

--- sorter.pl
+++ sorter.pl
@@ -8,11 +8,16 @@ my $SORTING_dir;
 my $download_dir;
 my @extension = ('jpg', 'png', 'gif', 'webm'); 
 
-if ($^O == "linux") {
+print "OS is - $^O\n";
+
+if ($^O =~ "linux") {
 	$download_dir = '/home/myacer/Downloads';
 	$SORTING_dir = "$download_dir\/SORTING";
+} elsif ($^O =~ "MSWin32"){
+	$download_dir = 'C:\Users\Dima\Downloads';
+	$SORTING_dir = "$download_dir\\SORTING";
 } else {
-	print "do nothing\n";
+	print "Unknown OS\n";
 }
 
 #Check directory exists - if not, create
@@ -27,7 +32,7 @@ if (-e $SORTING_dir && -x $SORTING_dir && -d $SORTING_dir) {
 opendir (S_DIR, $download_dir) or die $!;
 opendir (D_DIR, $SORTING_dir) or die $!;
 
-if ($^O == "linux") {
+if ($^O =~ "linux") {
 	#Read files from linux-directory
 	while(my $fname = readdir S_DIR) {
     		my $path_to_file = "$download_dir\/$fname";
@@ -50,8 +55,31 @@ if ($^O == "linux") {
 			}
 		}
 	}
+} elsif ($^O =~ "MSWin32") {
+	#Read files from win-directory
+	while(my $fname = readdir S_DIR) {
+    		my $path_to_file = "$download_dir\\$fname";
+		#Parse files by extension
+		for (my $i = 0; $i <= $#extension; $i++) {
+			if ($fname =~ /$\.$extension[$i]/) {
+				#If directory is directory && available && exist
+				if (-e "$SORTING_dir\\$extension[$i]" && -x "$SORTING_dir\\$extension[$i]" && -d "$SORTING_dir\\$extension[$i]") {
+					copy("$download_dir\/$fname", "$SORTING_dir\/$extension[$i]");
+					if (-e "$SORTING_dir\\$extension[$i]\\$fname") {
+						unlink ("$download_dir\\$fname");
+					} else {
+						print "Can't copy $fname\n $!\n";
+					}
+					print "Move $fname to $extension[$i]-folder\n";
+				} else {
+					mkdir "$SORTING_dir\\$extension[$i]";
+					print "Directory $SORTING_dir\\$extension[$i] was created!\n";
+				}
+			}
+		}
+	}
 } else {
-	print "do_nothing\n";
+	print "#DoNnothing\n";
 }
 
 #Close all folders
